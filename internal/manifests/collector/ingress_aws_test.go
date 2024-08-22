4f88a8338a91d48882af8ff0db800eb9305bad3e
Dmitry Sobolev <dmitry@dmitrys-mbp.lan>
2024-08-22 17:49:06 -0400

    moar tests for ingress

--- internal/manifests/collector/ingress_aws_test.go
+++ internal/manifests/collector/ingress_aws_test.go
@@ -16,6 +16,7 @@ package collector
 
 import (
 	_ "embed"
+	"errors"
 	"sort"
 	"testing"
 
@@ -96,11 +97,188 @@ func TestDesiredIngressesAws(t *testing.T) {
 
 		params.OtelCol.Namespace = ns
 		params.OtelCol.Spec.Ingress = v1beta1.Ingress{
-			Type:               v1beta1.IngressTypeIngress,
-			Hostname:           hostname,
-			Annotations:        map[string]string{"some.key": "some.value"},
-			IngressClassName:   &ingressClassName,
-			CollectorEndpoints: map[string]string{"otlp/1": "otlp-1.some.domain.io", "otlp/2": "otlp-2.some.domain.io"},
+			Type:             v1beta1.IngressTypeIngress,
+			Hostname:         hostname,
+			Annotations:      map[string]string{"some.key": "some.value"},
+			IngressClassName: &ingressClassName,
+			CollectorEndpoints: map[string]string{
+				"otlp/1": "otlp-1.some.domain.io",
+				"otlp/2": "otlp-2.some.domain.io",
+				"jaeger": "jaeger.some.domain.io",
+			},
+		}
+
+		got, err := IngressAws(params)
+		assert.NoError(t, err)
+
+		pathType := networkingv1.PathTypePrefix
+
+		ingressRulesGot := got.Spec.Rules
+		ingressRulesExpected := []networkingv1.IngressRule{
+			{
+				Host: "otlp-1.some.domain.io",
+				IngressRuleValue: networkingv1.IngressRuleValue{
+					HTTP: &networkingv1.HTTPIngressRuleValue{
+						Paths: []networkingv1.HTTPIngressPath{
+							{
+								Path:     "/opentelemetry.proto.collector.logs.v1.LogsService",
+								PathType: &pathType,
+								Backend: networkingv1.IngressBackend{
+									Service: &networkingv1.IngressServiceBackend{
+										Name: "test-collector-grpc",
+										Port: networkingv1.ServiceBackendPort{
+											Name: "otlp-1-grpc",
+										},
+									},
+								},
+							},
+							{
+								Path:     "/opentelemetry.proto.collector.traces.v1.TracesService",
+								PathType: &pathType,
+								Backend: networkingv1.IngressBackend{
+									Service: &networkingv1.IngressServiceBackend{
+										Name: "test-collector-grpc",
+										Port: networkingv1.ServiceBackendPort{
+											Name: "otlp-1-grpc",
+										},
+									},
+								},
+							},
+							{
+								Path:     "/opentelemetry.proto.collector.metrics.v1.MetricsService",
+								PathType: &pathType,
+								Backend: networkingv1.IngressBackend{
+									Service: &networkingv1.IngressServiceBackend{
+										Name: "test-collector-grpc",
+										Port: networkingv1.ServiceBackendPort{
+											Name: "otlp-1-grpc",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			{
+				Host: "otlp-2.some.domain.io",
+				IngressRuleValue: networkingv1.IngressRuleValue{
+					HTTP: &networkingv1.HTTPIngressRuleValue{
+						Paths: []networkingv1.HTTPIngressPath{
+							{
+								Path:     "/opentelemetry.proto.collector.logs.v1.LogsService",
+								PathType: &pathType,
+								Backend: networkingv1.IngressBackend{
+									Service: &networkingv1.IngressServiceBackend{
+										Name: "test-collector-grpc",
+										Port: networkingv1.ServiceBackendPort{
+											Name: "otlp-2-grpc",
+										},
+									},
+								},
+							},
+							{
+								Path:     "/opentelemetry.proto.collector.traces.v1.TracesService",
+								PathType: &pathType,
+								Backend: networkingv1.IngressBackend{
+									Service: &networkingv1.IngressServiceBackend{
+										Name: "test-collector-grpc",
+										Port: networkingv1.ServiceBackendPort{
+											Name: "otlp-2-grpc",
+										},
+									},
+								},
+							},
+							{
+								Path:     "/opentelemetry.proto.collector.metrics.v1.MetricsService",
+								PathType: &pathType,
+								Backend: networkingv1.IngressBackend{
+									Service: &networkingv1.IngressServiceBackend{
+										Name: "test-collector-grpc",
+										Port: networkingv1.ServiceBackendPort{
+											Name: "otlp-2-grpc",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			{
+				Host: "jaeger.some.domain.io",
+				IngressRuleValue: networkingv1.IngressRuleValue{
+					HTTP: &networkingv1.HTTPIngressRuleValue{
+						Paths: []networkingv1.HTTPIngressPath{
+							{
+								Path:     "/jaeger.api_v2/CollectorService",
+								PathType: &pathType,
+								Backend: networkingv1.IngressBackend{
+									Service: &networkingv1.IngressServiceBackend{
+										Name: "test-collector-grpc",
+										Port: networkingv1.ServiceBackendPort{
+											Name: "jaeger-grpc",
+										},
+									},
+								},
+							},
+							{
+								Path:     "/jaeger.api_v3/QueryService",
+								PathType: &pathType,
+								Backend: networkingv1.IngressBackend{
+									Service: &networkingv1.IngressServiceBackend{
+										Name: "test-collector-grpc",
+										Port: networkingv1.ServiceBackendPort{
+											Name: "jaeger-grpc",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+
+		assert.True(t, len(ingressRulesExpected) == len(ingressRulesGot))
+		for i := 0; i < len(ingressRulesExpected); i++ {
+			sort.Slice(ingressRulesGot, func(i, j int) bool { return ingressRulesGot[i].Host < ingressRulesGot[j].Host })
+			sort.Slice(ingressRulesExpected, func(i, j int) bool { return ingressRulesExpected[i].Host < ingressRulesExpected[j].Host })
+			assert.True(t, ingressRulesExpected[i].Host == ingressRulesGot[i].Host)
+		}
+
+		for i := 0; i < len(ingressRulesExpected); i++ {
+			pathsGot := ingressRulesGot[i].IngressRuleValue.HTTP.Paths
+			pathsExpected := ingressRulesExpected[i].IngressRuleValue.HTTP.Paths
+			sort.Slice(pathsGot, func(i, j int) bool { return pathsGot[i].Path < pathsGot[j].Path })
+			sort.Slice(pathsExpected, func(i, j int) bool { return pathsExpected[i].Path < pathsExpected[j].Path })
+
+			assert.True(t, ingressRulesExpected[i].Host == ingressRulesGot[i].Host)
+			assert.Equal(t, ingressRulesExpected, ingressRulesGot)
+		}
+	})
+	t.Run("multiple grpc receivers, ONE host-to-component mapping record absent", func(t *testing.T) {
+		var (
+			ns               = "test"
+			hostname         = "example.com"
+			ingressClassName = "aws"
+		)
+
+		params, err := newParams("something:tag", testFileIngressAws)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		params.OtelCol.Namespace = ns
+		params.OtelCol.Spec.Ingress = v1beta1.Ingress{
+			Type:             v1beta1.IngressTypeIngress,
+			Hostname:         hostname,
+			Annotations:      map[string]string{"some.key": "some.value"},
+			IngressClassName: &ingressClassName,
+			CollectorEndpoints: map[string]string{
+				"otlp/1": "otlp-1.some.domain.io",
+				"otlp/2": "otlp-2.some.domain.io",
+			},
 		}
 
 		got, err := IngressAws(params)
@@ -202,9 +380,12 @@ func TestDesiredIngressesAws(t *testing.T) {
 			},
 		}
 
-		assert.Equal(t, len(ingressRulesExpected), len(ingressRulesGot))
-		assert.True(t, ingressRulesExpected[0].Host == ingressRulesGot[0].Host)
-		assert.True(t, ingressRulesExpected[1].Host == ingressRulesGot[1].Host)
+		assert.True(t, len(ingressRulesExpected) == len(ingressRulesGot))
+		for i := 0; i < len(ingressRulesExpected); i++ {
+			sort.Slice(ingressRulesGot, func(i, j int) bool { return ingressRulesGot[i].Host < ingressRulesGot[j].Host })
+			sort.Slice(ingressRulesExpected, func(i, j int) bool { return ingressRulesExpected[i].Host < ingressRulesExpected[j].Host })
+			assert.True(t, ingressRulesExpected[i].Host == ingressRulesGot[i].Host)
+		}
 
 		for i := 0; i < len(ingressRulesExpected); i++ {
 			pathsGot := ingressRulesGot[i].IngressRuleValue.HTTP.Paths
@@ -212,7 +393,34 @@ func TestDesiredIngressesAws(t *testing.T) {
 			sort.Slice(pathsGot, func(i, j int) bool { return pathsGot[i].Path < pathsGot[j].Path })
 			sort.Slice(pathsExpected, func(i, j int) bool { return pathsExpected[i].Path < pathsExpected[j].Path })
 
+			assert.True(t, ingressRulesExpected[i].Host == ingressRulesGot[i].Host)
 			assert.Equal(t, ingressRulesExpected, ingressRulesGot)
 		}
+	})
+	t.Run("multiple grpc receivers, ALL host-to-component mapping records absent", func(t *testing.T) {
+		var (
+			ns               = "test"
+			hostname         = "example.com"
+			ingressClassName = "aws"
+		)
+
+		params, err := newParams("something:tag", testFileIngressAws)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		params.OtelCol.Namespace = ns
+		params.OtelCol.Spec.Ingress = v1beta1.Ingress{
+			Type:               v1beta1.IngressTypeIngress,
+			Hostname:           hostname,
+			Annotations:        map[string]string{"some.key": "some.value"},
+			IngressClassName:   &ingressClassName,
+			CollectorEndpoints: map[string]string{},
+		}
+
+		got, err := IngressAws(params)
+		assert.Error(t, err)
+		assert.Equal(t, err, errors.New("empty components to hostnames mapping"))
+		assert.Nil(t, got)
 	})
 }
